28b988bf62dd52e42beea25459dd48732fc06978
Kristina <nice.kres@mail.ru>
2023-04-12 19:03:49 +0300

    feat: made what-season.js and dream-team.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,11 +14,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(backyard) {
+  let counter = 0;
+  for(let elem of backyard) {
+    for (let e of elem) {
+      console.log(e)
+      if(e==='^^'){
+        counter++;
+      }
+    }
+  }
+  return counter;
 }
 
+console.log(countCats([[0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2]]))
 module.exports = {
   countCats
 };
